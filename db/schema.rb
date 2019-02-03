d2706c44168080a15c8cb9c26f5a4efe40d9b055
Zachary Lehmann <zlehmann@gmail.com>
2019-02-03 15:32:10 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190203152853) do
+ActiveRecord::Schema.define(version: 20190203153104) do
 
   create_table "artists", force: :cascade do |t|
     t.string   "name"
@@ -22,8 +22,8 @@ ActiveRecord::Schema.define(version: 20190203152853) do
   create_table "preferences", force: :cascade do |t|
     t.string   "song_sort_order"
     t.string   "artist_sort_order"
-    t.string   "allow_create_songs"
-    t.string   "allow_create_artists"
+    t.boolean  "allow_create_songs"
+    t.boolean  "allow_create_artists"
     t.datetime "created_at",           null: false
     t.datetime "updated_at",           null: false
   end
